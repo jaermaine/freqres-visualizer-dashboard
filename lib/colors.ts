7cc79e6129bfc7e56b120b504c7dbb386d95640c
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2026-07-19 19:50:34 +0800

    Fix missing colors module

---
+++ lib/colors.ts
@@ -0,0 +1,11 @@
+export const PALETTE = [
+  "#ff00aa", // Neon Pink
+  "#00ffcc", // Neon Teal
+  "#ffea00", // Neon Yellow
+  "#b700ff", // Neon Purple
+  "#ff4d00", // Neon Orange
+  "#00d4ff", // Neon Blue
+  "#a2ff00", // Neon Lime
+  "#ff88a8", // Light Pink
+  "#88b3ff", // Light Blue
+];
